a78f79d111718dfdde110cd4459ae2fd936ab0a0
Mohamed Mufasil Mufeeth <mufasilmuf@gmail.com>
2022-09-11 21:44:24 +0530

    Exercise 4.3

---
+++ Week-4/Exercise-4.3.js
@@ -0,0 +1,20 @@
+const Fib = (n) => ({
+    [Symbol.iterator]: () => {
+        let i = 1, old = 0, new1 = 0;
+        return {
+            next() {
+                if (i++ <= n) {
+                    [old, new1] = [new1, (old + new1) || 1];
+                    return { value: old, done: false }
+                }
+                else {
+                    return { done: true }
+                }
+            }
+        }
+    }
+})
+
+for (let num of Fib(6)) {
+    console.log(num);
+}
